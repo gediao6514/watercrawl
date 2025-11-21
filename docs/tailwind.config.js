533a05c33ca3f0feb45771b5671728490276cf72
孙龙山 <sunlongshan@sunlongshandeMac-mini.local>
2025-11-22 02:35:57 +0800

    feat: add tailwindcss support (config + postcss + directives)

---
+++ docs/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: ['class', '[data-theme="dark"]'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx,md,mdx}',
+    './docs/**/*.{md,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
